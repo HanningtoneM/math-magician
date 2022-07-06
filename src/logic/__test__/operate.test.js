f2fd70069065aa1898fa6aaa5d897c16f1e02724
maen <maenkh@yahoo.com>
2022-07-06 16:30:44 +0300

    add subtraction test to operate

--- src/logic/__test__/operate.test.js
+++ src/logic/__test__/operate.test.js
@@ -3,6 +3,9 @@ describe('Testing operate.js file', () => {
   it('Test sum 3 + 3 = 6', () => {
     expect(operate('3', '3', '+')).toBe('6');
   });
+  it('Test subtraction 10 - 3 = 7', () => {
+    expect(operate('10', '3', '-')).toBe('7');
+  });
   it('Test multiply 3 * 3 = 9', () => {
     expect(operate('3', '3', 'x')).toBe('9');
   });
